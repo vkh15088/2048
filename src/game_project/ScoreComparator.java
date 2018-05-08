7d87a380af4ff05c4c2b38b78918e10838d690d9
duongbaohung0309 <34331783+duongbaohung0309@users.noreply.github.com>
2018-05-08 15:24:15 +0700

    Add files via upload

--- src/game_project/ScoreComparator.java
+++ src/game_project/ScoreComparator.java
@@ -2,11 +2,12 @@ package game_project;
 
 import java.util.Comparator;
 
-public class ScoreComparator implements Comparator<Score> {// comparator tu java va lay thong tin tu class Score
+public class ScoreComparator implements Comparator<Score> {
         public int compare(Score score1, Score score2) {
 
             int sc1 = score1.getScore();
-            int sc2 = score2.getScore();// descending
+            int sc2 = score2.getScore();
+
             if (sc1 > sc2){
                 return -1;
             }else if (sc1 < sc2){
